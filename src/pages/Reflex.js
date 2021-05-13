af5ef18c1dff78055b3ecd34e3e6201c841e5673
Oinari-Shinto <oinari.shinto@gmail.com>
2021-05-13 23:58:00 +0300

    Create pages and path to

---
+++ src/pages/Reflex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Wester = () => {
+    return (
+        <div>
+            <h1>Reflex</h1>
+            <p>Расширительные баки для отопления и водоснаюжения Reflex</p>
+            <p>Производство Германия</p>
+        </div>
+    )
+}
+
+export default Wester
