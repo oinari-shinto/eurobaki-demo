88f5cd048f415b8c85aff9e63376f208e341168a
Oinari-Shinto <oinari.shinto@gmail.com>
2021-05-29 13:54:02 +0300

    create scroll anime and test hook anime

--- src/components/animeComponents/HeroH1.js
+++ src/components/animeComponents/HeroH1.js
@@ -1,46 +1,89 @@
-import React from 'react'
-import { useSpring, animated } from 'react-spring'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useSpring, animated, Spring } from 'react-spring';
+import styled from 'styled-components';
+import VisibilitySensor from "react-visibility-sensor";
 
 
 
-export default function HeroH1({HeaderMark}) {
+// export default function HeroH1({HeaderMark}) {
     
-    const fadeIn = useSpring(
-        {
-            from: { opacity: 0, transform: 'translateY(3rem)' },
-            to: { opacity: 1, transform: 'translateY(-1rem)' },
-            config: { duration: 1000 }
-        }
-    )
+//     const fadeIn = useSpring(
+//         {
+//             from: { opacity: 0, transform: 'translateY(3rem)' },
+//             to: { opacity: 1, transform: 'translateY(-1rem)' },
+//             config: { duration: 1000 }
+//         }
+//     )
+const FadeInDirection = ({ isVisible, children }) => {
+    const props = useSpring({
+      opacity: isVisible ? 1 : 0,
+      transform: isVisible ? "translateY(0px)" : "translateY(50px)"
+    });
+    return <animated.div style={props}>{children}</animated.div>;
+  };
+  
+  export const FadeInContainer = ({ children }) => {
+    const [isVisible, setVisibility] = useState(false);
+  
+    const onChange = visiblity => {
+      visiblity && setVisibility(visiblity);
+    };
+  
+    return (
+      <VisibilitySensor onChange={onChange}>
+        <FadeInDirection isVisible={isVisible}>{children}</FadeInDirection>
+      </VisibilitySensor>
+    );
+  };
 
-    if (HeaderMark) {
+      export default function HeroH1({HeaderMark}) {
         return (
+          <div>
+            
+            <FadeInContainer>
+            <HeaderH1>{HeaderMark}</HeaderH1>
+            </FadeInContainer>
+            
+           
+       
+          </div>
+        );
+      }
+
+//     if (HeaderMark) {
+//         return (
+            
+
+            
+//         )
+//     }
+//     else 
+//     {
+//         return (
         
-            <animated.div style={fadeIn}>
+//             <animated.div style={fadeIn}>
+//                 <HeaderH1>ありがとうございます</HeaderH1>
+//                 <HeroP>Завод производство. Доставка по России</HeroP>                
+//             </animated.div>
+//         )
+//     }
+    
+    
+// }
+
+
+
+
+
+
+
+{/* <animated.div style={fadeIn}>
                 <HeaderH1>                
                     {HeaderMark}                    
                 </HeaderH1>
                 <HeroP>Завод производство. Доставка по России</HeroP>
                 
-            </animated.div>
-        )
-    }
-    else 
-    {
-        return (
-        
-            <animated.div style={fadeIn}>
-                <HeaderH1>ありがとうございます</HeaderH1>
-                <HeroP>Завод производство. Доставка по России</HeroP>                
-            </animated.div>
-        )
-    }
-    
-    
-}
-
-
+            </animated.div> */}
 
 
 const HeaderH1 = styled.h1`
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,13 +5,14 @@ import { useSpring, animated  } from 'react-spring';
 import Layout from "../layouts/mainLayout"
 import SEO from "../components/seo"
 import Component1 from "../components/Component1"
+import Testor from "../components/Testor"
 
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <Component1/>
-    
+    <Testor/>
     
     
     <Link to="/">Go back to the homepage</Link>
