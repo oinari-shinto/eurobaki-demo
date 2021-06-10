7194a3da5ec703d05eecc6dd56c4afa4898ebd74
Oinari-Shinto <oinari.shinto@gmail.com>
2021-06-10 23:46:42 +0300

    fix.0.91 Certifican section fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,45 +11,19 @@ import Stats from "../components/Stats"
 import Email from "../components/Email"
 import Footer from "../components/Footer"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TableWester from "../components/dataTables/TableWester"
-import TableReflex from "../components/dataTables/TableReflex"
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger);
-gsap.defaults({ease: "none", duration: 2});
+
+
 
 
 
 
 const IndexPage = () =>  {
-    const tl = gsap.timeline();
-    useEffect(() => {
-      
-      tl.from('.westerTable', {xPercent: -100})
-        .from('.reflexTable', {xPercent: 100});
-    
-    ScrollTrigger.create({
-      animation: tl,
-      trigger: ".container",
-      start: "top top",
-      end: "+=4000",
-      scrub: true,
-      pin: true,
-      anticipatePin: 1
-    })
-    }, [])
-    
-
 
     return (
     <Layout >
       <SEO title="Home" />
       <Hero HeaderMark="Продажа и изготовление расширительных баков"/>
       <Products heading="Выберите расширительный бак"/>
-      <div className="container">
-        <TableWester className="westerTable"/>
-        {/* <TableReflex className="reflexTable"/> */}
-      </div>
       <Certificates  />
       <Stats  />
       <Email />
